59b8ae7f63d76a0cd3e6924137b1f3c88f6f4f3f
gemersonhenry <henryingtel@gmail.com>
2017-04-15 23:32:23 +0100

    feat(App): variables, expresiones, comentarios, sentencias

--- src/MiBundle/Controller/DefaultController.php
+++ src/MiBundle/Controller/DefaultController.php
@@ -8,6 +8,19 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MiBundle:Default:index.html.twig');
+    	$productos = array(
+    			array( "producto" => "Prod. #1", "precio" => 2 ),
+    			array( "producto" => "Prod. #2", "precio" => 6 ),
+    			array( "producto" => "Prod. #3", "precio" => 4 ),
+    			array( "producto" => "Prod. #4", "precio" => 5 ),
+    			array( "producto" => "Prod. #5", "precio" => 9 ),
+    		);
+
+        return $this->render('MiBundle:Default:index.html.twig',
+        	array(
+        		'title' => "TÍTULO DE MI APLICACIÓN",
+        		'productos' => $productos
+        	)
+        );
     }
 }
